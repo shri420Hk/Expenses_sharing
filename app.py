71fd68a5aa6a6a34e50bf93ac7f9f773a8400258
SRIKANT <76119503+shri420Hk@users.noreply.github.com>
2024-07-28 15:47:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     with app.app_context(): 
         db.create_all()
     
-    # Import routes after creating database
+    
     from routes import init_routes
     init_routes(app)
     app.run(debug=True)
